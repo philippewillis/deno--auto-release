bbdb1bbfbf485063f6fdba5ce1c52efeed1482ec
philopian <philopian@gmail.com>
2025-06-06 13:50:31 -0700

    feat: Add compression middleware to Hono application

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { compress } from "hono/compress";
 
 export function createApp() {
   const app = new Hono();
 
   // Middleware
+  app.use(compress());
   app.use("*", cors());
 
   // Routes
