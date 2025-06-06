789c103f31f0c11cb147351af12850dcdf2958b4
philopian <philopian@gmail.com>
2025-06-06 13:52:00 -0700

    feat: Add logger middleware to Hono application

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { compress } from 'hono/compress'
+import { logger } from 'hono/logger'
 
 export function createApp() {
   const app = new Hono()
@@ -8,6 +9,7 @@ export function createApp() {
   // Middleware
   app.use(compress())
   app.use('*', cors())
+  app.use(logger())
 
   // Routes
   app.get('/', (c) => c.text('Hono!'))
